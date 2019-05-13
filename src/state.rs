6c08c687af8c088819c27ea117fa096f64406d0a
Jon Haggblad <jon.haggblad@gmail.com>
2019-05-13 15:22:51 +0200

    chore/flows_dst: extract out flows_elder

--- src/state.rs
+++ src/state.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use crate::{actions::*, flows_dst::*, flows_node::*, flows_src::*, utilities::*};
+use crate::{actions::*, flows_dst::*, flows_elder::*, flows_node::*, flows_src::*, utilities::*};
 use std::{
     collections::{BTreeMap, BTreeSet},
     fmt::{self, Display, Formatter},
